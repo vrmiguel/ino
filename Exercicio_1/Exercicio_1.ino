e416450be6ead3dee229b261d0dc0cf226b0dafc
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-08-24 20:51:40 -0300

    Adiciona o exercício 1

---
+++ Exercicio_1/Exercicio_1.ino
@@ -0,0 +1,59 @@
+const int ledPin = LED_BUILTIN;
+const int buzzerPin = 2;
+
+int ledState = LOW;
+int buzzerState = LOW;
+
+int State = 0;
+
+
+unsigned long previousMillis = 0;
+
+const long interval = 100000;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  
+  Serial.begin(9600);
+  Serial.println(State);
+
+}
+
+void loop() {
+
+
+  if (millis() - previousMillis >= interval) {
+    previousMillis = millis();
+    State++;
+    State = State % 3;
+    
+    Serial.println(State);
+    
+    switch (State) {
+      case 0:
+        buzzerState = LOW;
+        ledState = LOW;
+        
+        break;
+        
+      case 1: 
+        ledState = HIGH;
+        buzzerState = LOW;
+        break;
+        
+      default:
+        ledState = HIGH;
+        buzzerState = HIGH;
+        break;
+    }
+    
+    digitalWrite(buzzerPin, buzzerState);
+    digitalWrite(ledPin, ledState);
+  }
+
+
+}
