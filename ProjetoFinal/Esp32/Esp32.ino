b0ea458a5d7fb29f431d83d42712ec0cef1c3a7b
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-12-01 00:16:49 -0300

    ProjetoFinal(ESP32): migrate to MQTT

--- ProjetoFinal/Esp32/Esp32.ino
+++ ProjetoFinal/Esp32/Esp32.ino
@@ -1,15 +1,21 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
-#include <coap-simple.h>
+#include <PubSubClient.h>
 
-WiFiUDP udp;
-Coap coap(udp);
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+const char *MQTT_SERVER = "broker.hivemq.com";
+const char *MQTT_TOPIC = "/telemetry";
+const uint16_t MQTT_PORT = 1883;
 
 const char* ssid = "miniIoT";
 const char* password = "12345678";
 
 int last_updated_sensors = 0;
 int sensor_update_interval = 1000;
+
+int last_updated_mqtt = 0;
+int mqtt_update_interval = 1000;
 
 // Dados de sensores, vindos do Arduino
 int temperature;
@@ -32,22 +38,26 @@ String check_sensor(char sensor) {
   return retorno;
 }
 
-// CoAP client response callback
-void callback_response(CoapPacket &packet, IPAddress ip, int port) {
-  char p[packet.payloadlen + 1];
-  memcpy(p, packet.payload, packet.payloadlen);
-  p[packet.payloadlen] = NULL;
-
-  Serial.print("Recebi resposta: ");
-  Serial.println(p);
+void ConnectToMqtt() {
+  Serial.println("Connecting to MQTT Broker...");
+  while (!mqttClient.connected()) {
+    char clientId[100] = "\0";
+    sprintf(clientId, "ESP32Client-%04X", random(0xffff));
+    Serial.print("Conectando ao MQTT, clientID: ");
+    Serial.println(clientId);
+    if (mqttClient.connect(clientId)) {
+      Serial.println("Conectando ao broker MQTT");
+      mqttClient.subscribe(MQTT_TOPIC);
+    }
+  }
 }
 
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
 
-//  /pinMode(led_interno, OUTPUT);
-// / pinMode(buzzer, OUTPUT);
+  //  /pinMode(led_interno, OUTPUT);
+  // / pinMode(buzzer, OUTPUT);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -59,11 +69,6 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  // Define a função na qual recebemos respostas do servidor
-  coap.response(callback_response);
-
-  coap.start();
 }
 
 // Atualize os valores de sensor com o Arduino
@@ -83,14 +88,15 @@ void update_sensors() {
 
 // Atualiza o dashboard com os valores de sensor
 void update_dashboard() {
-  String body = "{";
-  body += "temperature:200";
-  body += "}";
+  char * body = (char*) malloc(1024 * 2);
 
-  Serial.println("Envie esta bomba pro ThingsBoard");
-  coap.put(IPAddress(44, 196, 172, 187), 5683, "api/v1/mbxQvYcGHTeTAFJBsZZH/telemetry", body.c_str());
-  
-  Serial.println("Envie esta bomba pro ThingsBoard");
+  sprintf(body, "{temperature:%d,luminosity:%d,is_raining:%d,is_near:%d}", temperature, luminosity, is_raining, is_near);
+
+  Serial.print("Enviando dados de sensor para o ThingsBoard..");
+
+  mqttClient.publish(MQTT_TOPIC, body);
+
+  Serial.println(".. pronto!");
 }
 
 void loop() {
@@ -101,7 +107,13 @@ void loop() {
     update_dashboard();
   }
 
-  coap.loop();
+  if (ms - last_updated_mqtt >= mqtt_update_interval) {
+    if (!mqttClient.connected()) {
+      ConnectToMqtt();
+    }
+    mqttClient.loop();
+  }
+
 
   delay(1500);
 }
