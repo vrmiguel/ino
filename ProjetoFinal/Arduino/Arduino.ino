dcfe6a34b19515d8aac55d64294582392c254c82
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-12-05 20:57:19 -0300

    ProjetoFinal: updated code

--- ProjetoFinal/Arduino/Arduino.ino
+++ ProjetoFinal/Arduino/Arduino.ino
@@ -1,25 +1,28 @@
+
 const int MEASURE_COUNT = 5;
 
-const int motion_sensor_pin = A0;
-const int rain_sensor_pin = A1;
-const int lm35_pin = 2;
-const int ldr_pin = 3;
+const int motion_sensor_pin = 3;
+const int rain_sensor_pin = 5;
+const int lm35_pin = A1;
+const int ldr_pin = A0;
 
 int last_updated_sensors = 0;
-int sensor_update_interval = 1000;
+int sensor_update_interval = 5000;
 
 int is_raining;
 int is_near;
-int lumens;
-float temp_celsius;
 
 int temperature_avg[MEASURE_COUNT] = {0};
 int luminosity_avg[MEASURE_COUNT] = {0};
 
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600);
 
   pinMode(motion_sensor_pin, INPUT);
+  pinMode(rain_sensor_pin, INPUT);
+
+  Serial.println("Iniciado!");
 }
 
 void push_arr(int * arr, int new_value) {
@@ -30,6 +33,14 @@ void push_arr(int * arr, int new_value) {
   arr[0] = new_value;
 }
 
+int get_average(const int * arr) {
+  int sum = 0;
+  for (int i = 0; i < MEASURE_COUNT; i++) {
+    sum += arr[i];
+  }
+
+  return sum / MEASURE_COUNT;
+}
 
 float lm35_read_volts() {
   int step_value = analogRead(lm35_pin);
@@ -48,38 +59,60 @@ float lm35_read_temperature() {
 }
 
 void loop() {
-  delay(2500);
-
   int ms = millis();
   if (ms - last_updated_sensors >= sensor_update_interval) {
+    Serial.println("Atualizando sensores!");
     // Atualização de sensores
 
-    int is_raining = digitalRead(rain_sensor_pin);
-    int is_near = digitalRead(motion_sensor_pin);
-    int lumen_reading = analogRead(ldr_pin);
-    int temp_reading = lm35_read_temperature();
+    is_raining = digitalRead(rain_sensor_pin);
+    Serial.print("Chove?");
+    Serial.println(is_raining ? "Sim" : "Nao");
     
+    is_near = digitalRead(motion_sensor_pin);
+    Serial.print("Motorista no carro?");
+    Serial.println(is_near ? "Sim" : "Nao");
+
+    
+    int lumen_reading = analogRead(ldr_pin);
+    Serial.print("Luminancia:");
+    Serial.println(lumen_reading);
+
+    
+    int temp_reading = lm35_read_temperature();
+    Serial.print("Temperatura:");
+    Serial.println(temp_reading);
+
     push_arr(luminosity_avg, lumen_reading);
     push_arr(temperature_avg, temp_reading);
+
+    
+
+    last_updated_sensors = ms;
   }
 
   if (Serial2.available()) {
+    Serial.println("Pedido do ESP32 recebido");
     char chooser = Serial2.read();
 
     if (chooser == 't') {
       // t -> temperature
-      String temperature = String(temp_celsius, DEC);
+      
+      String temperature = String(get_average(temperature_avg), DEC);
       Serial2.print(temperature);
+      Serial.print(temperature);
     } else if (chooser == 'r') {
       // r -> rain sensor
       Serial2.print(is_raining);
+      Serial.print(is_raining);
     } else if (chooser == 'm') {
       // m -> motion sensor
-      Serial2.print(is_near);      
+      Serial2.print(is_near);
+      Serial.print(is_near);      
     } else if (chooser == 'l') {
       // l -> lumens
-      String luminosity = String(lumens, DEC);
+      String luminosity = String(get_average(luminosity_avg), DEC);
       Serial2.print(luminosity);
+      Serial.print(luminosity);
     }
   }
 }
--- ProjetoFinal/Esp32/Esp32.ino
+++ ProjetoFinal/Esp32/Esp32.ino
@@ -4,12 +4,15 @@
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
-const char *MQTT_SERVER = "broker.hivemq.com";
-const char *MQTT_TOPIC = "/telemetry";
+const char *MQTT_SERVER = "mqtt.thingsboard.cloud";
+const char *MQTT_TOPIC = "v1/devices/me/telemetry";
+const char *MQTT_CLIENT_ID = "wk4ult0143kbgyuwvlgo";
+const char *MQTT_USERNAME = "cyj5dkus0rnsgjl9bl6x";
+const char *MQTT_PASSWORD = "of8jh40a96tc96q2832s";
 const uint16_t MQTT_PORT = 1883;
 
-const char* ssid = "miniIoT";
-const char* password = "12345678";
+const char* ssid = "Crabstation";
+const char* password = "senhasenha";
 
 int last_updated_sensors = 0;
 int sensor_update_interval = 1000;
@@ -24,9 +27,21 @@ int is_raining;
 int is_near;
 
 String check_sensor(char sensor) {
+  if(sensor == 'r') {
+    Serial.print("Sensor de chuva: ");
+  } else if (sensor == 'm') {
+    Serial.print("Sensor de proximidade: ");
+  } else if (sensor == 't') {
+    Serial.print("Temperatura: ");
+  } else if (sensor == 'l') {
+    Serial.print("Lumens: ");
+  } else {
+    Serial.print("Desconhecido");
+  }
+  
   String retorno;
 
-  Serial2.write(sensor);
+  Serial2.print(sensor);
 
   // Espera o Arduino responder
   while (!Serial2.available()) {}
@@ -41,12 +56,10 @@ String check_sensor(char sensor) {
 void ConnectToMqtt() {
   Serial.println("Connecting to MQTT Broker...");
   while (!mqttClient.connected()) {
-    char clientId[100] = "\0";
-    sprintf(clientId, "ESP32Client-%04X", random(0xffff));
     Serial.print("Conectando ao MQTT, clientID: ");
-    Serial.println(clientId);
-    if (mqttClient.connect(clientId)) {
-      Serial.println("Conectando ao broker MQTT");
+    Serial.println(MQTT_CLIENT_ID);
+    if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+      Serial.println("Conectado ao broker MQTT");
       mqttClient.subscribe(MQTT_TOPIC);
     }
   }
@@ -65,6 +78,8 @@ void setup() {
     Serial.print(".");
   }
 
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -74,16 +89,24 @@ void setup() {
 // Atualize os valores de sensor com o Arduino
 void update_sensors() {
   String temp = check_sensor('t');
+  Serial.print("Valor lido: ");
+  Serial.println(temperature);
   temperature = temp.toInt();
 
   String lumens = check_sensor('l');
   luminosity = lumens.toInt();
+  Serial.print("Valor lido: ");
+  Serial.println(luminosity);
 
   String rain = check_sensor('r');
   is_raining = rain.toInt();
+  Serial.print("Valor lido: ");
+  Serial.println(is_raining);
 
   String near = check_sensor('m');
   is_near = near.toInt();
+  Serial.print("Valor lido: ");
+  Serial.println(near);
 }
 
 // Atualiza o dashboard com os valores de sensor
@@ -92,6 +115,8 @@ void update_dashboard() {
 
   sprintf(body, "{temperature:%d,luminosity:%d,is_raining:%d,is_near:%d}", temperature, luminosity, is_raining, is_near);
 
+  Serial.print("Request body: ");
+  Serial.println(body);
   Serial.print("Enviando dados de sensor para o ThingsBoard..");
 
   mqttClient.publish(MQTT_TOPIC, body);
@@ -103,17 +128,19 @@ void loop() {
   int ms = millis();
 
   if (ms - last_updated_sensors >= sensor_update_interval) {
-    // update_sensors();
-    update_dashboard();
+    update_sensors();
+
+    last_updated_sensors = ms;
   }
 
   if (ms - last_updated_mqtt >= mqtt_update_interval) {
     if (!mqttClient.connected()) {
       ConnectToMqtt();
     }
-    mqttClient.loop();
+
+    update_dashboard();
+    last_updated_mqtt = ms;
   }
 
-
-  delay(1500);
+  mqttClient.loop();
 }
